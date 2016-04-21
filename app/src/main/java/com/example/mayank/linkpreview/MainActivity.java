f0b2a8b19f15f8f36e8effb9724c470d16e0057a
mayank26saxena <mayank26saxena@gmail.com>
2016-04-22 04:18:01 +0530

    Title, Description, Logo set into cardview.

--- app/src/main/java/com/example/mayank/linkpreview/MainActivity.java
+++ app/src/main/java/com/example/mayank/linkpreview/MainActivity.java
@@ -1,21 +1,24 @@
 package com.example.mayank.linkpreview;
 
 import android.app.Activity;
-import android.content.DialogInterface;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -29,6 +32,16 @@ public class MainActivity extends Activity {
     String s;
     URL url;
 
+    ProgressDialog mProgressDialog;
+
+    String previewTitle;
+    String previewDescription;
+    Bitmap previewLogo;
+
+    TextView mTitleTv;
+    TextView mDescriptionTv;
+    ImageView mLogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +49,11 @@ public class MainActivity extends Activity {
 
         mEditText = (EditText) findViewById(R.id.urlLinkTv);
         mButton = (Button) findViewById(R.id.findPreviewButton);
+
+        View cardview_layout = findViewById(R.id.card_view);
+        mTitleTv = (TextView) cardview_layout.findViewById(R.id.title);
+        mDescriptionTv = (TextView) cardview_layout.findViewById(R.id.description);
+        mLogo = (ImageView) cardview_layout.findViewById(R.id.thumbnail);
 
 
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +65,13 @@ public class MainActivity extends Activity {
                     url = new URL(s);
                     Log.d(TAG, "Success in forming URL.");
                     Log.d(TAG, "URL FOUND. URL = " + url);
+                    new Title().execute();
+                    new Description().execute();
+                    new Logo().execute();
 
-                    ( new ParseURL() ).execute(new String[]{s});
+                    mTitleTv.setText(previewTitle);
+                    mDescriptionTv.setText(previewDescription);
+                    mLogo.setImageBitmap(previewLogo);
 
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
@@ -57,70 +80,122 @@ public class MainActivity extends Activity {
 
             }
         });
-
     }
 
-
-    private class ParseURL extends AsyncTask<String, Void, String> {
-
-
-        @Override
-        protected String doInBackground(String... strings) {
-            StringBuffer buffer = new StringBuffer();
-            try {
-                Log.d("JSwa", "Connecting to [" + strings[0] + "]");
-                Document doc = Jsoup.connect(strings[0]).get();
-                Log.d("JSwa", "Connected to [" + strings[0] + "]");
-                // Get document (HTML page) title
-                String title = doc.title();
-                Log.d("JSwA", "Title [" + title + "]");
-                buffer.append("Title: " + title + "\r\n");
-
-                // Get meta info
-                Elements metaElems = doc.select("meta");
-                buffer.append("META DATA\r\n");
-                for (Element metaElem : metaElems) {
-                    String name = metaElem.attr("name");
-                    String content = metaElem.attr("content");
-                    buffer.append("name [" + name + "] - content [" + content + "] \r\n");
-                }
-
-                Elements topicList = doc.select("h2.topic");
-                buffer.append("Topic list\r\n");
-                for (Element topic : topicList) {
-                    String data = topic.text();
-
-                    buffer.append("Data [" + data + "] \r\n");
-                }
-
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-
-            return buffer.toString();
-        }
+    // Title AsyncTask
+    private class Title extends AsyncTask<Void, Void, Void> {
+        String title;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Please wait");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
         }
 
         @Override
-        protected void onPostExecute(String data) {
-            super.onPostExecute(data);
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(String.valueOf(url)).get();
+                // Get the html document title
+                title = document.title();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-
-            new AlertDialog.Builder(getApplicationContext())
-                    .setTitle("Link Preview")
-                    .setMessage(data)
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            Log.d(TAG, "User pressed okay.");
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set title into previewTitle
+            previewTitle  = title;
+            mProgressDialog.dismiss();
         }
     }
+
+    // Description AsyncTask
+    private class Description extends AsyncTask<Void, Void, Void> {
+        String desc;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Please wait");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(String.valueOf(url)).get();
+                // Using Elements to get the Meta data
+                Elements description = document
+                        .select("meta[name=description]");
+                // Locate the content attribute
+                desc = description.attr("content");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set desc into previewDescription
+            previewDescription = desc;
+            mProgressDialog.dismiss();
+        }
+    }
+
+    // Logo AsyncTask
+    private class Logo extends AsyncTask<Void, Void, Void> {
+        Bitmap bitmap;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Please wait");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(String.valueOf(url)).get();
+                // Using Elements to get the class data
+                Elements img = document.select("a[class=brand brand-image] img[src]");
+                // Locate the src attribute
+                String imgSrc = img.attr("src");
+                // Download image from URL
+                InputStream input = new java.net.URL(imgSrc).openStream();
+                // Decode Bitmap
+                bitmap = BitmapFactory.decodeStream(input);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set downloaded image into previewLogo
+            previewLogo = bitmap;
+            mProgressDialog.dismiss();
+        }
+    }
+
 }
